209aec8f5b369a450266727f4628a5eaf179b625
Jakub Wąsik <32049761+Eghizio@users.noreply.github.com>
2024-10-13 01:58:48 +0200

    Connect Pixabay search to form

--- src/js/hw-11.js
+++ src/js/hw-11.js
@@ -3,17 +3,17 @@ document
   .addEventListener('submit', event => {
     event.preventDefault();
 
-    const query = event.target.elements['query'].value;
+    const searchQuery = event.target.elements['query'].value;
 
-    console.log({ query });
+    getPhotos(searchQuery).then(console.log);
   });
 
 const API_KEY = 'my_secret_pixabay_api_key';
 
-const getPhotos = () => {
+const getPhotos = searchQuery => {
   const params = new URLSearchParams({
     key: API_KEY,
-    q: 'dogs',
+    q: searchQuery,
     image_type: 'photo',
     orientation: 'horizontal',
     safesearch: true,
