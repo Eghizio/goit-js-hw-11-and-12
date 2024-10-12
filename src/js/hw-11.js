59b15ab624284b5a102a85c37c05a1c24e83c0b6
Jakub Wąsik <32049761+Eghizio@users.noreply.github.com>
2024-10-13 01:56:53 +0200

    Pixabay API setup

--- src/js/hw-11.js
+++ src/js/hw-11.js
@@ -7,3 +7,19 @@ document
 
     console.log({ query });
   });
+
+const API_KEY = 'my_secret_pixabay_api_key';
+
+const getPhotos = () => {
+  const params = new URLSearchParams({
+    key: API_KEY,
+    q: 'dogs',
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+  });
+
+  return fetch(`pixabay_api_endpoint_url?${params}`)
+    .then(response => response.json())
+    .catch(console.error);
+};
